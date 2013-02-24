c48f694701edf2eb3cddeedaaa891f4e82d74943
kuna <kgdgwn98@gmail.com>
2013-02-24 21:23:12 +0900

    first commit

---
+++ WiFi_Server_CSharp/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace WiFi_Server_CSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lb = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.acclX = new System.Windows.Forms.ProgressBar();
+            this.acclY = new System.Windows.Forms.ProgressBar();
+            this.acclZ = new System.Windows.Forms.ProgressBar();
+            this.accl = new System.Windows.Forms.ProgressBar();
+            this.accl_stat = new System.Windows.Forms.Label();
+            this.ori_stat = new System.Windows.Forms.Label();
+            this.pb = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Server";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lb
+            // 
+            this.lb.FormattingEnabled = true;
+            this.lb.ItemHeight = 12;
+            this.lb.Location = new System.Drawing.Point(12, 45);
+            this.lb.Name = "lb";
+            this.lb.Size = new System.Drawing.Size(437, 244);
+            this.lb.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(112, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 27);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Close Server";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(212, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 27);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Send Data";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // acclX
+            // 
+            this.acclX.Location = new System.Drawing.Point(12, 295);
+            this.acclX.Maximum = 10000;
+            this.acclX.Name = "acclX";
+            this.acclX.Size = new System.Drawing.Size(169, 18);
+            this.acclX.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.acclX.TabIndex = 2;
+            // 
+            // acclY
+            // 
+            this.acclY.Location = new System.Drawing.Point(12, 319);
+            this.acclY.Maximum = 10000;
+            this.acclY.Name = "acclY";
+            this.acclY.Size = new System.Drawing.Size(169, 18);
+            this.acclY.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.acclY.TabIndex = 3;
+            // 
+            // acclZ
+            // 
+            this.acclZ.Location = new System.Drawing.Point(12, 343);
+            this.acclZ.Maximum = 10000;
+            this.acclZ.Name = "acclZ";
+            this.acclZ.Size = new System.Drawing.Size(169, 18);
+            this.acclZ.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.acclZ.TabIndex = 4;
+            // 
+            // accl
+            // 
+            this.accl.Location = new System.Drawing.Point(12, 382);
+            this.accl.Maximum = 10000;
+            this.accl.Name = "accl";
+            this.accl.Size = new System.Drawing.Size(169, 18);
+            this.accl.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.accl.TabIndex = 5;
+            // 
+            // accl_stat
+            // 
+            this.accl_stat.AutoSize = true;
+            this.accl_stat.Location = new System.Drawing.Point(12, 409);
+            this.accl_stat.Name = "accl_stat";
+            this.accl_stat.Size = new System.Drawing.Size(38, 12);
+            this.accl_stat.TabIndex = 6;
+            this.accl_stat.Text = "label1";
+            // 
+            // ori_stat
+            // 
+            this.ori_stat.AutoSize = true;
+            this.ori_stat.Location = new System.Drawing.Point(227, 295);
+            this.ori_stat.Name = "ori_stat";
+            this.ori_stat.Size = new System.Drawing.Size(38, 12);
+            this.ori_stat.TabIndex = 7;
+            this.ori_stat.Text = "label1";
+            // 
+            // pb
+            // 
+            this.pb.Location = new System.Drawing.Point(229, 319);
+            this.pb.Name = "pb";
+            this.pb.Size = new System.Drawing.Size(220, 99);
+            this.pb.TabIndex = 8;
+            this.pb.TabStop = false;
+            this.pb.Paint += new System.Windows.Forms.PaintEventHandler(this.pbPaint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 430);
+            this.Controls.Add(this.pb);
+            this.Controls.Add(this.ori_stat);
+            this.Controls.Add(this.accl_stat);
+            this.Controls.Add(this.accl);
+            this.Controls.Add(this.acclZ);
+            this.Controls.Add(this.acclY);
+            this.Controls.Add(this.acclX);
+            this.Controls.Add(this.lb);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lb;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar acclX;
+        private System.Windows.Forms.ProgressBar acclY;
+        private System.Windows.Forms.ProgressBar acclZ;
+        private System.Windows.Forms.ProgressBar accl;
+        private System.Windows.Forms.Label accl_stat;
+        private System.Windows.Forms.Label ori_stat;
+        private System.Windows.Forms.PictureBox pb;
+    }
+}
+
---
+++ WiFi_Server_CSharp/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+// http://blog.daum.net/starkcb/46
+// Server Side
+
+namespace WiFi_Server_CSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InitalizeServer();
+        }
+
+        public static Socket Server, Client;
+        public bool InitalizeServer()
+        {
+            // set addr obj
+            IPAddress serverIP = IPAddress.Parse("192.168.0.108");
+            IPEndPoint serverEP = new IPEndPoint(serverIP, 1234);
+
+            try {
+                // set server obj
+                Server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                Server.Bind(serverEP);
+                Server.Listen(10);
+
+                Status("-------------------------------");
+                Status("Waiting for Connection ..."); 
+                Status("-------------------------------");
+                RunClientThread();
+
+            } catch (Exception e) {
+                MessageBox.Show(e.Message.ToString());
+                Status("Error Initalizing Server."); 
+            }
+
+            return true;
+        }
+
+        public static Thread SThread;
+        public void RunClientThread()
+        {
+            SThread = new Thread(SocketThreadFunc);
+            SThread.Start();
+        }
+
+        string stringbyte;
+        byte[] getByte = new byte[1024];
+        public void SocketThreadFunc()
+        {
+            stringbyte = null;
+
+            Client = Server.Accept();
+            if (!Client.Connected)
+            {
+                Status("연결에 실패하였습니다");
+                return;
+            }
+            Status("연결되었습니다");
+
+            while (Client.Connected)
+            {
+                getByte = new byte[1024];
+                int l = Client.Receive(getByte);
+                stringbyte = Encoding.ASCII.GetString(getByte);
+                String[] str_datas = stringbyte.Split('\n');
+                for (int i=0; i<str_datas.Length; i++)
+                    ProcessRecvData(str_datas[i]);
+            }
+            Status("연결 종료");
+        }
+
+        public void ProcessRecvData(String str)
+        {
+            try
+            {
+                str = str.Replace("\0", string.Empty);
+                if (str.Length == 0) return;
+
+                Status(str);
+                String[] str_param = str.Split('#');
+                if (str_param.Length <= 1)
+                {
+                    Status(String.Format("데이터:{0}", str_param[0]));
+
+                }
+                else if (str_param.Length == 3)
+                {
+                    if (Int32.Parse(str_param[0]) == 200)
+                    {
+                        String[] str_args = str_param[2].Split(',');
+                        float _acclX = Single.Parse(str_args[0]);
+                        float _acclY = Single.Parse(str_args[1]);
+                        float _acclZ = Single.Parse(str_args[2]);
+
+                        acclX.Invoke(new MethodInvoker(delegate() { acclX.Value = (int)(_acclX * 100) + 5000; }));
+                        acclY.Invoke(new MethodInvoker(delegate() { acclY.Value = (int)(_acclY * 100) + 5000; }));
+                        acclZ.Invoke(new MethodInvoker(delegate() { acclZ.Value = (int)(_acclZ * 100) + 5000; }));
+
+                        accl_stat.Invoke(new MethodInvoker(delegate() { accl_stat.Text = sensor_Accl_proc(_acclX, _acclY, _acclZ).ToString(); }));
+                    }
+                    else if (Int32.Parse(str_param[0]) == 201)
+                    {
+                        String[] str_args = str_param[2].Split(',');
+                        oriX = Single.Parse(str_args[0]);
+                        oriY = Single.Parse(str_args[1]);
+                        oriZ = Single.Parse(str_args[2]);
+
+                        ori_stat.Invoke(new MethodInvoker(delegate()
+                        {
+                            ori_stat.Text = String.Format("{0}, {1}, {2}",
+                                oriX, oriY, oriZ);
+                        }));
+                        pb.Invalidate();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Status("[ERROR] 와이파이 수신상태가 좋지 않습니다 (Buffer Overflow)");
+            }
+        }
+
+        public static int byteArrayDefrag(byte[] sData)
+        {
+            int endLength = 0;
+            for (int i = 0; i < sData.Length; i++)
+            {
+                if ((byte)sData[i] != (byte)0)
+                {
+                    endLength = i;
+                }
+            }
+            return endLength;
+        }
+
+        public void Status(String s)
+        {
+            lb.Invoke(new MethodInvoker(delegate()
+            {
+                lb.Items.Add(s);
+            }));
+        }
+
+        public float sensor_Accl_proc(float X, float Y, float Z)
+        {
+            double v = System.Math.Sqrt(X*X+Y*Y+Z*Z);
+            return (float)(v - 9.8);
+        }
+
+        float oriX;
+        float oriY;
+        float oriZ;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SThread.Abort();
+            Server.Close();
+            Client.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String str = "Shake\n";
+            ASCIIEncoding ae = new ASCIIEncoding();
+
+            //Server.Send(ae.GetBytes(str));
+            int i = Client.Send(ae.GetBytes(str));
+            Status(String.Format("{0} bytes 보냄", i));
+        }
+
+        private void pbPaint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(Color.Black);
+
+            // get pos
+            int px = (int)(pb.Width * (1 + oriX / 40) / 2);
+            int py = (int)(pb.Height * (1 + oriY / 30) / 2);
+
+            e.Graphics.DrawRectangle(new Pen(Color.White), px - 5, py - 5, 10, 10);
+        }
+
+        /*
+         * 할 일
+         * 1. 센서값을 화면좌표로 환산 (ok)
+         * -> calibration 기능 추가! (ok)
+         * 2. 패킷송수신 예외 제거 및 확실하게 보장하기
+         * 3. 가속도센서 확인해보기 (일정값 이상이면 특정 이벤트 작동) <선택> -> 왼쪽 오른쪽 위 아래 앞 뒤 가속도상태확인하기
+         *  
+         */
+    }
+}

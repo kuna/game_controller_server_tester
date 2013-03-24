854d9fabfb852db875f74e6a2b46eec7128e0d2e
kuna <kgdgwn98@gmail.com>
2013-03-24 17:05:58 +0900

    key input

--- WiFi_Server_CSharp/Form1.cs
+++ WiFi_Server_CSharp/Form1.cs
@@ -28,18 +28,19 @@ namespace WiFi_Server_CSharp
             InitalizeServer();
         }
 
-        public static Socket Server, Client;
+        public static Socket Server;
+        EndPoint localEP, remoteEP;
         public bool InitalizeServer()
         {
             // set addr obj
-            IPAddress serverIP = IPAddress.Parse("192.168.0.108");
-            IPEndPoint serverEP = new IPEndPoint(serverIP, 1234);
+            //IPAddress serverIP = IPAddress.Parse("192.168.0.108");
+            localEP = new IPEndPoint(IPAddress.Any, 1234);
+            remoteEP = new IPEndPoint(IPAddress.None, 1234);
 
             try {
                 // set server obj
-                Server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                Server.Bind(serverEP);
-                Server.Listen(10);
+                Server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                Server.Bind(localEP);
 
                 Status("-------------------------------");
                 Status("Waiting for Connection ..."); 
@@ -57,32 +58,32 @@ namespace WiFi_Server_CSharp
         public static Thread SThread;
         public void RunClientThread()
         {
+            bAlive = true;
             SThread = new Thread(SocketThreadFunc);
             SThread.Start();
         }
 
         string stringbyte;
         byte[] getByte = new byte[1024];
+        Boolean bAlive;
         public void SocketThreadFunc()
         {
-            stringbyte = null;
-
-            Client = Server.Accept();
-            if (!Client.Connected)
+            try
             {
-                Status("연결에 실패하였습니다");
-                return;
+                while (bAlive)
+                {
+                    getByte = new byte[1024];
+                    int l = Server.ReceiveFrom(getByte, ref remoteEP);
+                    stringbyte = Encoding.ASCII.GetString(getByte);
+                    String[] str_datas = stringbyte.Split('\n');
+                    for (int i = 0; i < str_datas.Length; i++)
+                        ProcessRecvData(str_datas[i]);
+                }
             }
-            Status("연결되었습니다");
-
-            while (Client.Connected)
+            catch (Exception e)
             {
-                getByte = new byte[1024];
-                int l = Client.Receive(getByte);
-                stringbyte = Encoding.ASCII.GetString(getByte);
-                String[] str_datas = stringbyte.Split('\n');
-                for (int i=0; i<str_datas.Length; i++)
-                    ProcessRecvData(str_datas[i]);
+                MessageBox.Show(e.Message.ToString());
+                Status("Error Socket Data Transmission."); 
             }
             Status("연결 종료");
         }
@@ -94,7 +95,6 @@ namespace WiFi_Server_CSharp
                 str = str.Replace("\0", string.Empty);
                 if (str.Length == 0) return;
 
-                Status(str);
                 String[] str_param = str.Split('#');
                 if (str_param.Length <= 1)
                 {
@@ -114,7 +114,11 @@ namespace WiFi_Server_CSharp
                         acclY.Invoke(new MethodInvoker(delegate() { acclY.Value = (int)(_acclY * 100) + 5000; }));
                         acclZ.Invoke(new MethodInvoker(delegate() { acclZ.Value = (int)(_acclZ * 100) + 5000; }));
 
-                        accl_stat.Invoke(new MethodInvoker(delegate() { accl_stat.Text = sensor_Accl_proc(_acclX, _acclY, _acclZ).ToString(); }));
+                        accl_stat.Invoke(new MethodInvoker(delegate() { 
+                            accl_stat.Text = String.Format("{0} - {1}, {2}, {3}",
+                                sensor_Accl_proc(_acclX, _acclY, _acclZ).ToString(),
+                                _acclX, _acclY, _acclZ); 
+                        }));
                     }
                     else if (Int32.Parse(str_param[0]) == 201)
                     {
@@ -129,6 +133,18 @@ namespace WiFi_Server_CSharp
                                 oriX, oriY, oriZ);
                         }));
                         pb.Invalidate();
+                    }
+                    else if (Int32.Parse(str_param[0]) == 300)
+                    {
+                        if (str_param[2] != "null")
+                        {
+                            Status(str_param[2]);
+                        }
+
+                        // left right up 등 만들기
+                    }
+                    else if (Int32.Parse(str_param[0]) == 301)
+                    {
                     }
                 }
             }
@@ -171,18 +187,18 @@ namespace WiFi_Server_CSharp
 
         private void button2_Click(object sender, EventArgs e)
         {
+            bAlive = false;
             SThread.Abort();
             Server.Close();
-            Client.Close();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            String str = "Shake\n";
+            String str = "Shake";
             ASCIIEncoding ae = new ASCIIEncoding();
 
             //Server.Send(ae.GetBytes(str));
-            int i = Client.Send(ae.GetBytes(str));
+            int i = Server.SendTo(ae.GetBytes(str), ae.GetBytes(str).Length, SocketFlags.None, remoteEP);
             Status(String.Format("{0} bytes 보냄", i));
         }
 
@@ -203,7 +219,10 @@ namespace WiFi_Server_CSharp
          * -> calibration 기능 추가! (ok)
          * 2. 패킷송수신 예외 제거 및 확실하게 보장하기
          * 3. 가속도센서 확인해보기 (일정값 이상이면 특정 이벤트 작동) <선택> -> 왼쪽 오른쪽 위 아래 앞 뒤 가속도상태확인하기
-         *  
+         * 
+         * grav y값 음수 (대략 -3) : 왼쪽
+           양수 (+3) : 오른쪽 
+         * 
          */
     }
 }
---
+++ WiFi_Server_CSharp/UDPSocket.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace WiFi_Server_CSharp
+{
+    class UDPSocket
+    {
+        private Socket socket;
+        EndPoint localEP, remoteEP;
+        byte[] buffer = new byte[1024];
+        
+        void Initialize(string ip, int port)
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            localEP = new IPEndPoint(IPAddress.Any, port);
+            remoteEP = new IPEndPoint(IPAddress.None, port);
+
+            socket.Bind(localEP);
+
+            createSocketThread();
+        }
+
+        private Thread SThread;
+        private void createSocketThread()
+        {
+            SThread = new Thread(SocketThread);
+            SThread.Start();
+        }
+
+        private void SocketThread()
+        {
+            while (true)
+            {
+            }
+        }
+
+        public void disconnect()
+        {
+            SThread.Abort();
+        }
+    }
+}
